a6ee440197b2d87664533c2fb9e9731b5af1f68c
NiviJah <nivijah@gmail.com>
2014-11-10 21:52:04 +0200

    Demo btn and request translation btn

--- wp-content/themes/flatsome/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/flatsome/woocommerce/single-product/add-to-cart/simple.php
@@ -16,37 +16,48 @@ if ( ! $product->is_purchasable() ) return;
 
 <?php
 	// Availability
-	$availability = $product->get_availability();
+$availability = $product->get_availability();
 
-	if ($availability['availability']) :
-		echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
-    endif;
+if ($availability['availability']) :
+	echo apply_filters( 'woocommerce_stock_html', '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>', $availability['availability'] );
+endif;
 ?>
 
 <?php if ( $product->is_in_stock() ) : ?>
 
 	<?php do_action('woocommerce_before_add_to_cart_form'); ?>
 
-<form class="cart" method="post" enctype='multipart/form-data'>
-	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+	<form class="cart" method="post" enctype='multipart/form-data'>
+		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 
-	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+		<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
 
-	 	<button type="submit" class="single_add_to_cart_button button secondary"><?php echo $product->single_add_to_cart_text(); ?></button>
-
-		 	<?php
-	 		if ( ! $product->is_sold_individually() )
-	 			woocommerce_quantity_input( array(
-	 				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-	 				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
-	 			) );
-	 	?>
-
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-	</form>
+		<button type="submit" class="single_add_to_cart_button button secondary"><?php echo $product->single_add_to_cart_text(); ?></button>
 
 
-	<?php do_action('woocommerce_after_add_to_cart_form'); ?>
 
-<?php endif; ?>
+		<?php
+		if ( ! $product->is_sold_individually() )
+			woocommerce_quantity_input( array(
+				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
+				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
+				) );
+				?>
+
+				<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+			</form>
+
+
+			<?php do_action('woocommerce_after_add_to_cart_form'); ?>
+
+		<?php endif; ?>
+		<?php 
+		$link_to_demo = get_post_meta( get_the_ID(), 'demo_affiliate_link', true );
+				// check if the custom field has a value
+		if( ! empty( $link_to_demo ) ) :?>
+		<a href="<?php echo $link_to_demo ?>"><button type="text" class="link_to_demo button secondary"><?php _e('Demo','woothemes'); ?></button></a>
+		
+	<?php endif;?>
+	<?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); }  ?>
